5516cd7e87442240f084d28409644d7ce80785b1
Florian <florian@flova.de>
2022-12-28 17:23:16 +0100

    Use multiprocessing

--- marimbabot_vision/scripts/generate_data.py
+++ marimbabot_vision/scripts/generate_data.py
@@ -1,5 +1,6 @@
 import os
 import random
+from multiprocessing import Pool
 
 import tqdm
 from abjad import LilyPondFile, Staff, Voice
@@ -21,18 +22,23 @@ def generate_piece(num_notes=20):
     staff_1 = Staff([voice_1], name="Staff_1")
     return string, staff_1
 
+def generate_sample(i):
+    string, staff = generate_piece()
+    os.makedirs(f"{OUTPUT_DIR}/{i}", exist_ok=True)
+    lilypond_file = LilyPondFile(
+        items=[
+            """#(set-default-paper-size "a8" 'landscape)""",
+            "#(set-global-staff-size 16)",
+            staff,
+        ],
+    )
+    as_png(lilypond_file, f"{OUTPUT_DIR}/{i}/staff_1.png", resolution=100)
+    with open(f"{OUTPUT_DIR}/{i}/staff_1.txt", 'w') as f:
+        f.write(string)
 
 if __name__ == "__main__":
-    for i in tqdm.tqdm(range(NUM_SAMPLES)):
-        string, staff = generate_piece()
-        os.makedirs(f"{OUTPUT_DIR}/{i}", exist_ok=True)
-        lilypond_file = LilyPondFile(
-            items=[
-                """#(set-default-paper-size "a8" 'landscape)""",
-                "#(set-global-staff-size 16)",
-                staff,
-            ],
-        )
-        as_png(lilypond_file, f"{OUTPUT_DIR}/{i}/staff_1.png", resolution=100)
-        with open(f"{OUTPUT_DIR}/{i}/staff_1.txt", 'w') as f:
-            f.write(string)
+    ids = range(NUM_SAMPLES)
+    # Call generate_sample on ids with tqdm and multiprocessing (24 workers)
+    with Pool(24) as pool:
+        tqdm.tqdm(pool.imap(generate_sample, ids), total=len(ids))
+    
