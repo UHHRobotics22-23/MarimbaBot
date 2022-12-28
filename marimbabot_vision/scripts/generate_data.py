9b51d97e373b677d009eacc91d6eb6bcc654e967
Florian <florian@flova.de>
2022-12-28 22:18:39 +0100

    Rework sampling to obey the time signature.

--- marimbabot_vision/scripts/generate_data.py
+++ marimbabot_vision/scripts/generate_data.py
@@ -10,20 +10,38 @@ from numpy.random import choice
 NUM_SAMPLES = 10000
 NUM_WORKER = 24
 OUTPUT_DIR = "data"
+MIN_DURATION = 16 # 1/16th note
 
-def note_sampler():
-    note = random.choice(['c', 'd', 'e', 'f', 'g', 'a', 'b'])
-    octave = choice(["", "'", "''"], p=[0.1, 0.8, 0.1])
-    duration = random.choice(['16', '8', '4', '2', ''])
+def note_sampler(duration):
+    """Sample a note or rest given a duration"""
+    note = random.choice(['c', 'd', 'e', 'f', 'g', 'a', 'b', 'r'])
+    octave = choice(["", "'", "''"], p=[0.0, 0.8, 0.2]) if note != 'r' else ''
     return note + octave + duration
 
-def generate_piece(num_notes=20):
-    string = ' '.join([note_sampler() for _ in range(num_notes)])
+def bar_sampler():
+    """Sample a bar of notes"""
+    def sample_duration(durations, level=0):
+        """Randomly subdivides a list of durations into smaller durations"""
+        prop = 1/3
+        new_durations = []
+        for duration in durations: 
+            if duration < MIN_DURATION and random.random() > prop:
+                new_durations.extend(sample_duration([duration * 2, duration * 2], level + 1))
+            else:
+                new_durations.append(duration)
+        return new_durations
+
+    return ' '.join([note_sampler(str(duration)) for duration in sample_duration([1,])]) 
+
+def generate_piece(num_bars=3):
+    """Generate a piece of music"""
+    string = ' '.join([bar_sampler() for _ in range(num_bars)])
     voice_1 = Voice(string, name="Voice_1")
     staff_1 = Staff([voice_1], name="Staff_1")
     return string, staff_1
 
 def generate_sample(i):
+    """Generate a sample and save it to disk"""
     string, staff = generate_piece()
     os.makedirs(f"{OUTPUT_DIR}/{i}", exist_ok=True)
     lilypond_file = LilyPondFile(
@@ -33,7 +51,7 @@ def generate_sample(i):
             staff,
         ],
     )
-    as_png(lilypond_file, f"{OUTPUT_DIR}/{i}/staff_1.png", resolution=100)
+    as_png(lilypond_file, f"{OUTPUT_DIR}/{i}/staff_1.png", resolution=200)
     with open(f"{OUTPUT_DIR}/{i}/staff_1.txt", 'w') as f:
         f.write(string)
 
@@ -41,4 +59,4 @@ if __name__ == "__main__":
     # Call generate_sample on ids with tqdm and multiprocessing (lilypond is single threaded)
     with Pool(NUM_WORKER) as pool:
         list(tqdm.tqdm(pool.imap(generate_sample, range(NUM_SAMPLES)), total=NUM_SAMPLES))
-    
+  
