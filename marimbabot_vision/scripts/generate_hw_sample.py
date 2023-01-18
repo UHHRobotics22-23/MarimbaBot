2d37dd04923ba6901b1a31b40af316b185611a81
Juliane Röscheisen <57991801+scaredycode@users.noreply.github.com>
2023-01-18 12:00:29 +0100

    update description

--- marimbabot_vision/scripts/generate_hw_sample.py
+++ marimbabot_vision/scripts/generate_hw_sample.py
@@ -5,8 +5,10 @@ from PIL import Image, ImageDraw
 import re
 
 # generates handwritten music notation from text by selecting randomly from handwritten symbols
-# run like this: python generate_hw_sample.py <string to be converted>
-# example: python generate_hw_sample.py "f16 g16 a16 b16 c'16 d'16 e'16 f'16 g'16 a'16 b'16 c''16 d''16 e''16 f''16 g''16 r1 r1 r2 r2 r4 r4 r8 r8 r8 r8 r16 r16 r16 r16 r4 r2 a''8 b''8 c'''8 d'''8 e'''8 f'''8 g'''8 a'''8 g'''4 f'''4 e'''4 d'''4 c'''2 b''2 a''2 g''2 f''2 e''2 d''2 c''2 b'1 a'1 g'1 f'1 e'1 d'1"
+# run like this: python generate_hw_sample.py <target dir>
+# example: python generate_hw_sample.py ./data/1/
+# <target dir> contains file 'staff_1.txt' which contains the lilypond text notation for this sample.
+# handwritten sample will be stored in <target dir> 
 
 HW_SYMBOLS_DIR = './hw_notation'
 #OUTPUT_DIR = './hw_data'
