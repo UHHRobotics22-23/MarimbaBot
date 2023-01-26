5cfd79169e5c335dbef0388cd3231895cf179d3a
Christian Salamut <christian.salamut>
2023-01-26 15:48:08 +0100

    add hw dataset as input folder

--- marimbabot_vision/scripts/generate_dataset_full.sh
+++ marimbabot_vision/scripts/generate_dataset_full.sh
@@ -1,4 +1,5 @@
 python generate_data.py 10000
 python generate_hw_data.py
+python generate_augmented_data.py --INPUT_DIR data_hw --AUGMENT_OUTPUT_DIR data_hw_augmented
 python generate_data.py 10000
 python generate_augmented_data.py
