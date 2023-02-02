086b63c3fe605265db11e568b378d37e4a5e04e3
Christian Salamut <christian.salamut>
2023-02-02 14:37:10 +0100

    change arg design

--- marimbabot_vision/scripts/generate_hw_data.py
+++ marimbabot_vision/scripts/generate_hw_data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+from functools import partial
 import os
 import re
 import shutil
@@ -10,6 +11,7 @@ import tqdm
 from numpy.random import choice, randint
 from PIL import Image, ImageDraw
 
+# use import for default value
 from generate_data import OUTPUT_DIR as INPUT_DIR
 
 # generates handwritten music notation by selecting randomly from handwritten symbols
@@ -32,7 +34,7 @@ head_positions = {'c': 130, 'd': 125, 'e': 120, 'f': 115, 'g': 110, 'a': 105, 'b
 def check_space(image, x_pos, y_offset):
     # check if end of page is reached
     if x_pos > SAMPLE_WIDTH-50:
-        x_pos = 40 + randint(MIN_SYMBOL_DIST, MAX_SYMBOL_DIST)
+        x_pos = 40 + randint(args.min_symbol_dist, args.max_symbol_dist)
         y_offset += 90
         draw_staff(image, y_offset)
     return x_pos, y_offset
@@ -40,8 +42,8 @@ def check_space(image, x_pos, y_offset):
 def check_bar(image, x_pos, y_offset, duration_counter):
     # check if current bar is full
     if duration_counter == 1:
-        draw_symbol(image, f'{HW_SYMBOLS_DIR}/bar', (x_pos,50+y_offset))
-        x_pos += randint(MIN_SYMBOL_DIST, MAX_SYMBOL_DIST)
+        draw_symbol(image, f'{args.hw_symbols_dir}/bar', (x_pos,50+y_offset))
+        x_pos += randint(args.min_symbol_dist, args.max_symbol_dist)
         duration_counter = 0
         x_pos, y_offset = check_space(image, x_pos, y_offset)
     return x_pos, y_offset, duration_counter
@@ -57,18 +59,18 @@ def extend_staff(image, x_pos, y_pos, y_offset, is_flipped):
     # draw extra lines above staff for notes higher than g''
     if y_pos < 40:
         for y in range(y_pos if y_pos%10 else y_pos+5, 45, 10):
-            draw_symbol(image, f'{HW_SYMBOLS_DIR}/extra', (x_pos, y+y_offset))
+            draw_symbol(image, f'{args.hw_symbols_dir}/extra', (x_pos, y+y_offset))
 
     # draw extra lines below staff for notes lower than d'
     elif y_pos > 60 and not is_flipped:
         for y in range(y_pos+30 if y_pos%10 else y_pos-5, 85, -10):
-            draw_symbol(image, f'{HW_SYMBOLS_DIR}/extra', (x_pos, y+y_offset))
+            draw_symbol(image, f'{args.hw_symbols_dir}/extra', (x_pos, y+y_offset))
 
 def draw_staff(image, y_offset):
     draw = ImageDraw.Draw(image)
     for i in range(50, 100, 10):
         draw.line((30,i+y_offset, SAMPLE_WIDTH-30,i+y_offset), fill=(0, 0, 0, 255))
-    draw_symbol(image, f'{HW_SYMBOLS_DIR}/clef', (30,30+y_offset))
+    draw_symbol(image, f'{args.hw_symbols_dir}/clef', (30,30+y_offset))
 
 def draw_symbol(image, symbol_path, position, is_flipped=False):
     symbol_file = choice(os.listdir(symbol_path))
@@ -77,22 +79,22 @@ def draw_symbol(image, symbol_path, position, is_flipped=False):
         symbol_im = symbol_im.transpose(Image.Transpose.ROTATE_180)
     image.paste(symbol_im, position, symbol_im)
 
-def draw_note(image, position, is_flipped, duration):
+def draw_note(image, position, is_flipped, duration, args):
     # choose note with inwards facing flags if higher than a'
     if duration > 4 and is_flipped:
-        draw_symbol(image, f'{HW_SYMBOLS_DIR}/note/{duration}_inv', position, is_flipped)
+        draw_symbol(image, f'{args.hw_symbols_dir}/note/{duration}_inv', position, is_flipped)
     else:
-        draw_symbol(image, f'{HW_SYMBOLS_DIR}/note/{duration}', position, is_flipped)
+        draw_symbol(image, f'{args.hw_symbols_dir}/note/{duration}', position, is_flipped)
 
-def generate_sample_image():
+def generate_sample_image(args):
     image = Image.new('RGBA', (SAMPLE_WIDTH, SAMPLE_HEIGHT), (255, 255, 255, 255))
     draw_staff(image, 0)
-    draw_symbol(image, f'{HW_SYMBOLS_DIR}/time', (70, 50))
+    draw_symbol(image, f'{args.hw_symbols_dir}/time', (70, 50))
     return image
 
-def draw_piece(string, sample_name):
-    sample_im = generate_sample_image()
-    x_pos = 70 + randint(MIN_SYMBOL_DIST, MAX_SYMBOL_DIST) 
+def draw_piece(string, sample_name, args):
+    sample_im = generate_sample_image(args)
+    x_pos = 70 + randint(args.min_symbol_dist, args.max_symbol_dist) 
     y_offset = 0
     duration_counter = 0
     piece = [(n[0], n.count('\''), int((re.findall(r'\d+', n)[0]))) for n in string.split()]
@@ -101,25 +103,25 @@ def draw_piece(string, sample_name):
         x_pos, y_offset, duration_counter = check_bar(sample_im, x_pos, y_offset, duration_counter)
 
         if note == 'r':
-            draw_symbol(sample_im, f'{HW_SYMBOLS_DIR}/rest/{duration}', (x_pos,50+y_offset))
+            draw_symbol(sample_im, f'{args.hw_symbols_dir}/rest/{duration}', (x_pos,50+y_offset))
         else:
             y_pos, is_flipped = get_note_pose(note, octave)
             extend_staff(sample_im, x_pos, y_pos, y_offset, is_flipped)
-            draw_note(sample_im, (x_pos, y_pos+y_offset), is_flipped, duration)
+            draw_note(sample_im, (x_pos, y_pos+y_offset), is_flipped, duration, args)
             
-        x_pos += randint(MIN_SYMBOL_DIST, MAX_SYMBOL_DIST)
+        x_pos += randint(args.min_symbol_dist, args.max_symbol_dist)
         duration_counter += 1/duration
-    draw_symbol(sample_im, f'{HW_SYMBOLS_DIR}/bar', (x_pos,50+y_offset))
+    draw_symbol(sample_im, f'{args.hw_symbols_dir}/bar', (x_pos,50+y_offset))
 
-    os.makedirs(f'{OUTPUT_DIR}/{sample_name}', exist_ok=True)    
-    sample_im.save(f'{OUTPUT_DIR}/{sample_name}/{NAME_PREFIX}.png','PNG')
-    shutil.copyfile(f'{INPUT_DIR}/{sample_name}/{NAME_PREFIX}.txt', f'{OUTPUT_DIR}/{sample_name}/{NAME_PREFIX}.txt')
-    shutil.copyfile(f'{INPUT_DIR}/{sample_name}/{NAME_PREFIX}.ly', f'{OUTPUT_DIR}/{sample_name}/{NAME_PREFIX}.ly')
+    os.makedirs(f'{args.output_dir}/{sample_name}', exist_ok=True)    
+    sample_im.save(f'{args.output_dir}/{sample_name}/{args.name_prefix}.png','PNG')
+    shutil.copyfile(f'{args.input_dir}/{sample_name}/{args.name_prefix}.txt', f'{args.output_dir}/{sample_name}/{args.name_prefix}.txt')
+    shutil.copyfile(f'{args.input_dir}/{sample_name}/{args.name_prefix}.ly', f'{args.output_dir}/{sample_name}/{args.name_prefix}.ly')
 
-def render(sample):
-    with open(f'{INPUT_DIR}/{sample}/staff_1.txt', 'r') as f:
+def render(sample, args):
+    with open(f'{args.input_dir}/{sample}/staff_1.txt', 'r') as f:
         string = f.read()
-    draw_piece(string, sample)
+    draw_piece(string, sample, args)
 
 
 if __name__ == "__main__":
@@ -129,14 +131,10 @@ if __name__ == "__main__":
     parser.add_argument("--name_prefix", type=str, required=False, help="Name prefix for the generated output files.", default=NAME_PREFIX)
     parser.add_argument("--min_symbol_dist", type=int, required=False, help="Minimum distance between notes.", default=MIN_SYMBOL_DIST)
     parser.add_argument("--max_symbol_dist", type=int, required=False, help="Maximum distance between notes.", default=MAX_SYMBOL_DIST)
+    parser.add_argument("--input_dir", type=str, required=False, help="Folder for the input data.", default=INPUT_DIR)
+    parser.add_argument("--output_dir", type=str, required=False, help="Folder for the output data.", default=OUTPUT_DIR)
 
     args = parser.parse_args()
-    HW_SYMBOLS_DIR = args.hw_symbols_dir
-    NUM_WORKER = args.num_worker
-    NAME_PREFIX = args.name_prefix
-    MIN_SYMBOL_DIST = args.min_symbol_dist
-    MAX_SYMBOL_DIST = args.max_symbol_dist
 
-
-    with Pool(NUM_WORKER) as pool:
-        list(tqdm.tqdm(pool.imap(render, os.listdir(INPUT_DIR)), total=len(os.listdir(INPUT_DIR))))
+    with Pool(args.num_worker) as pool:
+        list(tqdm.tqdm(pool.imap(partial(render, args=args), os.listdir(args.input_dir)), total=len(os.listdir(args.input_dir))))
