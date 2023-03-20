bb0c44a24e4c0840eb04ff030e12851be354c8b2
Juphex <20795522+Juphex@users.noreply.github.com>
2023-03-20 11:40:29 +0100

    fix lilypond formatting and deletion of temp file

--- marimbabot_vision/src/visualization_node.py
+++ marimbabot_vision/src/visualization_node.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
+import os
 import tempfile
 
 import cv2
-import numpy as np
 import rospy
-from abjad import Block, LilyPondFile
+from abjad import Block, LilyPondFile, Staff, Voice
 from abjad.persist import as_png
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image as ROSImage
@@ -13,9 +13,14 @@ from std_msgs.msg import String
 
 
 def callbackVisionResults(sentence, args):
+    rospy.logdebug(f"Received sentence: {sentence.data}")
     pub = args
 
-    lilypond_string = f"\\relative {{{sentence}}}"
+    # generate abjad staff
+    voice = Voice(sentence.data)
+    staff = Staff([voice])
+
+    # generate lilypond file
     header_block = Block(name="header")
     header_block.tagline = "#ff"
     lilypond_file = LilyPondFile(
@@ -23,24 +28,29 @@ def callbackVisionResults(sentence, args):
             header_block,
             """#(set-default-paper-size "a8" 'landscape)""",
             "#(set-global-staff-size 16)",
-            lilypond_string,
+            staff,
         ]
     )
     # store lilypond as png temporarily
     temp_ = tempfile.TemporaryFile()
-    as_png(lilypond_file, temp_.name, resolution=200)
+    as_png(lilypond_file, str(temp_.name), remove_ly=True, resolution=200)
+    png_filename = str(temp_.name) + ".png"
 
     # read as PIL and convert to ROSImage
-    cv_image = cv2.imread(temp_.name)
+    cv_image = cv2.imread(png_filename, cv2.IMREAD_UNCHANGED)
     bridge = CvBridge()
     image_message = bridge.cv2_to_imgmsg(cv_image, encoding="passthrough")
 
     pub.publish(image_message)
+    # remove temporary file after publishing
+    # this is needed because tempfile creates a file with a random name 
+    # but abjad creates a new file adding the postfix .png/.ly
+    os.remove(png_filename)
 
 def listener():
     rospy.init_node('visualization_node')
 
-    pub = rospy.Publisher('detection_visualization', ROSImage)
+    pub = rospy.Publisher('detection_visualization', ROSImage, queue_size=200)
     rospy.Subscriber("vision_node/recognized_sentence", String, callbackVisionResults, callback_args=(pub))
 
     # spin() simply keeps python from exiting until this node is stopped
