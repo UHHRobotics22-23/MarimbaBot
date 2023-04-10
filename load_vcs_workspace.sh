a6b610211b0135774bb31e1a23592f6bdb0b00d7
iibrahimli <imranibrahimli98@gmail.com>
2023-04-10 10:53:39 +0400

    Install curl & git

--- load_vcs_workspace.sh
+++ load_vcs_workspace.sh
@@ -3,9 +3,10 @@
 # Script to load a workspace from a workspace.repos file
 # To be run in the ROS Industrial-CI docker image
 
-# install vcstool
-curl -s https://packagecloud.io/install/repositories/dirk-thomas/vcstool/script.deb.sh | sudo bash
+# install vcstool & deps
 sudo apt update -y
+sudo apt install -y curl git
+curl -s https://packagecloud.io/install/repositories/dirk-thomas/vcstool/script.deb.sh | sudo bash
 sudo apt install -y python3-vcstool
 
 # import workspace.repos file
