fb07814d84d55f0bf9dbcbac917bed7d83b3e321
Florian Vahl <florian@flova.de>
2023-04-14 15:07:50 +0200

    Fix git safe directory error

--- load_vcs_workspace.sh
+++ load_vcs_workspace.sh
@@ -9,6 +9,9 @@ sudo apt install -y curl git
 curl -s https://packagecloud.io/install/repositories/dirk-thomas/vcstool/script.deb.sh | sudo bash
 sudo apt install -y python3-vcstool
 
+# setup git so it does not fail due to missmatching owners during vcs import
+git config --global --add safe.directory /root/target_ws/src/marimbabot
+
 # import workspace.repos file
 cd /root/target_ws/src
 vcs import --skip-existing --recursive < marimbabot/workspace.repos
