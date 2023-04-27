9acc6f49038d06e87c51ba81987b7de3138d5027
Juliane Röscheisen <juliane2109@gmail.com>
2023-04-27 15:41:43 +0200

    add queue size to camera subscriber

--- marimbabot_vision/src/vision_node.py
+++ marimbabot_vision/src/vision_node.py
@@ -65,7 +65,7 @@ def listener(pre_processor, model):
     rospy.init_node('vision_node')
 
     notes_publisher = rospy.Publisher("~recognized_notes", String)
-    rospy.Subscriber("cv_camera_node/image_raw", ROSImage, callbackImage, callback_args=(pre_processor, model, notes_publisher))
+    rospy.Subscriber("cv_camera_node/image_raw", ROSImage, callbackImage, callback_args=(pre_processor, model, notes_publisher), queue_size=1)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
