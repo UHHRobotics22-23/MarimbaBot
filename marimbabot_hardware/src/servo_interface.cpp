fd08c22c7818287e9b92616967c4e3d52daa314c
cvhex <baz5443@studium-hamburg.de>
2023-06-01 14:40:02 +0200

    wifi_modification

--- marimbabot_hardware/src/servo_interface.cpp
+++ marimbabot_hardware/src/servo_interface.cpp
@@ -1,53 +1,26 @@
 #include "marimbabot_hardware/servo_interface.hpp"
-#include <bits/stdc++.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-   
-#define PORT     8080
-#define MAXLINE 1024
+
 ServoInterface::ServoState::ServoState(const std::string &servo_name) {
     name = servo_name;
 }
 
 // Initializing the serial connection and servo_state object
-ServoInterface::ServoInterface(ros::NodeHandle& node_handle, std::string & ipaddress, int port) : 
+ServoInterface::ServoInterface(ros::NodeHandle& node_handle, std::string &device, int baud) : 
     servo_state("mallet_finger") {
     
     ROS_INFO("Initializing servo interface");
 
-    int sockfd;
-    char buffer[MAXLINE];
-    const char *hello = "Hello from server";
-    struct sockaddr_in servaddr, cliaddr;
-       
-    // Creating socket file descriptor
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-       
-    memset(&servaddr, 0, sizeof(servaddr));
-    memset(&cliaddr, 0, sizeof(cliaddr));
-       
-    // Filling server information
-    servaddr.sin_family    = AF_INET; // IPv4
-    servaddr.sin_addr.s_addr = INADDR_ANY;
-    servaddr.sin_port = htons(port);
-       
-    // Bind the socket with the server address
-    if ( bind(sockfd, (const struct sockaddr *)&servaddr, 
-            sizeof(servaddr)) < 0 )
-    {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-
-
+    // Initializing the serial connection
+    arduino_serial.setPort(device);
+    arduino_serial.setBaudrate(baud);
+    auto timeout = serial::Timeout::simpleTimeout(1000);
+    arduino_serial.setTimeout(
+        timeout.inter_byte_timeout,
+        timeout.read_timeout_constant,
+        timeout.read_timeout_multiplier,
+        timeout.write_timeout_constant,
+        timeout.write_timeout_multiplier
+    );
 
     try_open_serial_port();
     
