31127f03e6bede34e6551460543920105c264c54
Juliane Röscheisen <juliane2109@gmail.com>
2023-06-08 14:32:52 +0200

    catch lilypond parser error

--- marimbabot_behavior/src/marimbabot_behavior/behavior_node.py
+++ marimbabot_behavior/src/marimbabot_behavior/behavior_node.py
@@ -5,12 +5,14 @@ import actionlib
 from std_msgs.msg import String
 from marimbabot_behavior.interpreter import read_notes
 from marimbabot_msgs.msg import HitSequenceAction
+from abjad.exceptions import LilyPondParserError
 
 class ActionDecider:
     def __init__(self):
         # the currently active note sequence
         # will be updated with the latest sentence from vision_node/recognized_sentence after the 'read' command was issued
         self.sentence = None
+        self.hit_sequence = None
 
         # publishes a response for the synthesized speech
         self.response_pub = rospy.Publisher('~response', String, queue_size=10) 
@@ -31,19 +33,21 @@ class ActionDecider:
                 # wait for the vision node to publish a recognized sentence
                 self.sentence = rospy.wait_for_message('vision_node/recognized_notes', String, timeout=5).data 
                 rospy.loginfo(f"recognized notes: {self.sentence}")
+                self.hit_sequence = read_notes(self.sentence)
                 self.response_pub.publish('Notes recognized. Say play to play the notes.')
             except rospy.ROSException:
                 rospy.logwarn('No notes recognized. Make sure the notes are readable and visible to the camera.')
                 self.response_pub.publish('No notes recognized. Make sure the notes are readable and visible to the camera.')
+            except LilyPondParserError:
+                rospy.logwarn('Lilypond string not valid. Make sure the note are valid and readable.')
+                self.response_pub.publish('Lilypond string not valid. Make sure the note are valid and readable.')
 
         elif command.data == 'play':
-            rospy.loginfo('playing notes')
             # if a sentence has been read via the 'read' command, publish it to behavior_node/play_sentence to signal that notes should be played
-            if self.sentence:
+            if self.hit_sequence:
                 rospy.loginfo(f"playing notes: {self.sentence}")
-                goal_hit_sequence = read_notes(self.sentence)
                 rospy.loginfo(f"goal_hit_sequence: {goal_hit_sequence}")
-                self.client.send_goal(goal_hit_sequence)
+                self.client.send_goal(hit_sequence)
                 # TODO: include tempo (for future commands: faster, slower)
             else:
                 rospy.logwarn('No notes to play. Say reed to read notes.')
