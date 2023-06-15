43b7c0a414473968529b4a4320d23ea001091cfa
Imran Ibrahimli <imranibrahimli98@gmail.com>
2023-06-15 15:14:05 +0200

    vel threshold based detector

--- marimbabot_simulation/src/detect_bar_contact.py
+++ marimbabot_simulation/src/detect_bar_contact.py
@@ -4,74 +4,76 @@
 
 import rospy
 from sensor_msgs.msg import JointState
+from std_msgs.msg import String
 
-import threshold_detector
-
-print
-
+# velocity threshold to count as a bar hit
+VEL_THRESHOLD = 0.01
 
 # names of bars we are interested in
 # corresponding joints are <bar_name>/joint
-# e.g. "bar_A4" <-> "bar_A4/joint"
-BAR_JOINT_NAMES = [
-  "bar_A4/joint",
-  "bar_A5/joint",
-  "bar_A6/joint",
-  "bar_As4/joint",
-  "bar_As5/joint",
-  "bar_As6/joint",
-  "bar_B4/joint",
-  "bar_B5/joint",
-  "bar_B6/joint",
-  "bar_C4/joint",
-  "bar_C5/joint",
-  "bar_C6/joint",
-  "bar_C7/joint",
-  "bar_Cs4/joint",
-  "bar_Cs5/joint",
-  "bar_Cs6/joint",
-  "bar_D4/joint",
-  "bar_D5/joint",
-  "bar_D6/joint",
-  "bar_Ds4/joint",
-  "bar_Ds5/joint",
-  "bar_Ds6/joint",
-  "bar_E4/joint",
-  "bar_E5/joint",
-  "bar_E6/joint",
-  "bar_F4/joint",
-  "bar_F5/joint",
-  "bar_F6/joint",
-  "bar_Fs4/joint",
-  "bar_Fs5/joint",
-  "bar_Fs6/joint",
-  "bar_G4/joint",
-  "bar_G5/joint",
-  "bar_G6/joint",
-  "bar_Gs4/joint",
-  "bar_Gs5/joint",
-  "bar_Gs6/joint",
+# e.g. "bar_a4" <-> "bar_a4/joint"
+bar_jOINT_NAMES = [
+  "bar_a4/joint",
+  "bar_a5/joint",
+  "bar_a6/joint",
+  "bar_ais4/joint",
+  "bar_ais5/joint",
+  "bar_ais6/joint",
+  "bar_b4/joint",
+  "bar_b5/joint",
+  "bar_b6/joint",
+  "bar_c4/joint",
+  "bar_c5/joint",
+  "bar_c6/joint",
+  "bar_c7/joint",
+  "bar_cis4/joint",
+  "bar_cis5/joint",
+  "bar_cis6/joint",
+  "bar_d4/joint",
+  "bar_d5/joint",
+  "bar_d6/joint",
+  "bar_dis4/joint",
+  "bar_dis5/joint",
+  "bar_dis6/joint",
+  "bar_e4/joint",
+  "bar_e5/joint",
+  "bar_e6/joint",
+  "bar_f4/joint",
+  "bar_f5/joint",
+  "bar_f6/joint",
+  "bar_fis4/joint",
+  "bar_fis5/joint",
+  "bar_fis6/joint",
+  "bar_g4/joint",
+  "bar_g5/joint",
+  "bar_g6/joint",
+  "bar_gis4/joint",
+  "bar_gis5/joint",
+  "bar_gis6/joint",
 ]
 
-detector = threshold_detector.ThresholdDetector(pos_threshold=0.1, vel_threshold=0.1)
+joint2note = {
+    joint: joint[4:joint.index("/")] for joint in bar_jOINT_NAMES
+}
 
-
-def log(msg, *args, **kwargs):
-    rospy.loginfo("[detect_bar_contact] " + msg, *args, **kwargs)
+pub = None
 
 
 def joint_states_callback(message):
-    # parse joints of interest into a dict
-    state = {}
-    for i, name in zip(message.name):
-        if name in BAR_JOINT_NAMES:
-            state[name] = threshold_detector.PosVel(message.position[i], message.velocity[i])
-    contacts = detector.update(state)
+    for i, name in enumerate(message.name):
+        if name not in joint2note:
+            continue
+        absvel = abs(message.velocity[i])
+        if absvel > VEL_THRESHOLD:
+            rospy.loginfo(f"Detected contact with {joint2note[name]} vel={absvel:.5f}")
+            pub.publish(joint2note[name])
     return
 
 
 def listener():
     rospy.init_node("detect_bar_contact")
+    global pub
+    pub = rospy.Publisher("sim_notes", String, queue_size=10)
     rospy.Subscriber("joint_states", JointState, joint_states_callback, queue_size=1)
     rospy.spin()
 
