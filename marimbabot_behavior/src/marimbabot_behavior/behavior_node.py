1477d8c0e7c1509489a6e7f7be8ea4c31f86985a
Juliane Röscheisen <juliane2109@gmail.com>
2023-07-05 11:32:04 +0200

    add faster/slower play/preview

--- marimbabot_behavior/src/marimbabot_behavior/behavior_node.py
+++ marimbabot_behavior/src/marimbabot_behavior/behavior_node.py
@@ -23,7 +23,7 @@ class ActionDecider:
         self.hit_sequence = None
 
         # publishes a response for the synthesized speech
-        self.response_pub = rospy.Publisher('~response', String, queue_size=10) 
+        self.response_pub = rospy.Publisher('~response', String, queue_size=10)
 
         # listens to the recognized commands from speech node
         self.command_sub = rospy.Subscriber('speech_node/command', Command, self.callback_command)
@@ -34,10 +34,6 @@ class ActionDecider:
         # action client to send the sentence to the lilypond_audio action server
         self.lilypond_audio_client = actionlib.SimpleActionClient('audio_from_lilypond', LilypondAudioAction)
 
-    def callback_command(self, command_msg):
-        command = command_msg.command
-        rospy.loginfo(f"received command: {command}")
-
     def update_hit_sequence(self):
         try:
             self.hit_sequence = read_notes(self.sentence)
@@ -45,8 +41,77 @@ class ActionDecider:
             rospy.logwarn('Lilypond string not valid. Make sure the note are valid and readable.')
             self.response_pub.publish('Lilypond string not valid. Make sure the note sequence is valid.')
 
-    def callback_command(self, command):
-        rospy.loginfo(f"received command: {command.data}")
+    def play(self):
+        rospy.loginfo(f"playing notes: {self.sentence}")
+        rospy.loginfo(f"goal_hit_sequence: {self.hit_sequence}")
+
+        # preparing the hit sequence message for the audio node
+            # hit_sequence_msg = HitSequence()
+            # hit_sequence_msg.header.stamp = rospy.Time.now()
+            # hit_sequence_msg.seq_id = self.id_counter
+            # hit_sequence_msg.seq = self.hit_sequenc
+
+        def planning_client_thread():
+             # Waits until the action server has started up and started
+            self.planning_client.wait_for_server()
+            # Sends the goal to the action server.
+            self.planning_client.send_goal(self.hit_sequence)
+            # Waits for the server to finish performing the action.
+            # Includes that the audio file is generated and was played
+            self.planning_clinet.wait_for_result()
+            # Prints out the result of executing the action
+            rospy.logdebug(f"Result from planning action server: {self.planning_client.get_result()}")
+            # Check if we have a success
+            if not self.planning_clinet.get_result().success:
+                self.response_pub.publish('The sequence could not be played. Try again later.')
+
+        # start thread to not block the main thread if the action server is not currently active
+        if self.planning_client.simple_state == actionlib.SimpleGoalState.DONE:
+            threading.Thread(target=planning_client_thread).start()
+        else:    
+            rospy.logwarn('The motion is busy. Try again later.')
+            self.response_pub.publish('The motion is busy. Try again later.')
+
+    def preview(self):
+        rospy.loginfo(f"playing audio preview of notes: {self.sentence}")
+
+        def audio_from_lilypond_client_thread():
+            # Waits until the action server has started up and started
+            self.lilypond_audio_client.wait_for_server()
+            # Sends the goal to the action server.
+            self.lilypond_audio_client.send_goal(LilypondAudioGoal(lilypond_string=String(data = self.sentence)))
+            # Waits for the server to finish performing the action.
+            # Includes that the audio file is generated and was played
+            self.lilypond_audio_client.wait_for_result()
+            # Prints out the result of executing the action
+            rospy.logdebug(f"Result from audio_from_lilypond action server: {self.lilypond_audio_client.get_result()}")
+            # Check if we have a success
+            if not self.lilypond_audio_client.get_result().success:
+                self.response_pub.publish('The audio preview could not be played. Try again later.')
+
+        # start thread to not block the main thread if the action server is not currently active
+        if self.lilypond_audio_client.simple_state == actionlib.SimpleGoalState.DONE:
+            threading.Thread(target=audio_from_lilypond_client_thread).start()
+        else:
+            rospy.logwarn('Preview is busy. Try again later.')
+            self.response_pub.publish('Preview is busy. Try again later.')
+
+    def change_tempo(self, faster=True):
+        tempo = re.findall('\\\\tempo 4 = [0-9]+', self.sentence)
+        if len(tempo) > 0:
+            tempo = tempo[0].split(' ')[-1]
+            self.sentence = re.sub('\\\\tempo 4 = [0-9]+', '\\\\tempo 4 = {}'.format(str(int(tempo) + 20)), self.sentence) if faster else re.sub('\\\\tempo 4 = [0-9]+', '\\\\tempo 4 = {}'.format(str(int(tempo) - 20)), self.sentence)
+        else:
+            self.sentence = '\\tempo 4 = 80 ' + self.sentence if faster else '\\tempo 4 = 40 ' + self.sentence
+
+        rospy.loginfo(f"updated notes: {self.sentence}")
+        self.update_hit_sequence()
+        rospy.logwarn('Tempo increased.' if faster else 'Tempo decreased.')
+        self.response_pub.publish('Tempo increased.' if faster else 'Tempo decreased.')
+
+    def callback_command(self, command_msg):
+        command = command_msg.command
+        rospy.loginfo(f"received command: {command}")
 
         # read notes on the whiteboard
         if command == 'Marimbabot read':
@@ -65,67 +130,39 @@ class ActionDecider:
         elif command == 'Marimbabot start playing':
             # if a note sequence has been read via the 'read' command and the corresponding hit sequence is valid, the hit sequence is send to the planning action server
             if self.hit_sequence:
-                # check if action server is busy
-                if self.planning_client.simple_state == actionlib.SimpleGoalState.DONE:
-                    # preparing the hit sequence message for the audio node
-                    # hit_sequence_msg = HitSequence()
-                    # hit_sequence_msg.header.stamp = rospy.Time.now()
-                    # hit_sequence_msg.seq_id = self.id_counter
-                    # hit_sequence_msg.seq = self.hit_sequence
-
-                    rospy.loginfo(f"playing notes: {self.sentence}")
-                    rospy.loginfo(f"goal_hit_sequence: {self.hit_sequence}")
-                    self.planning_client.send_goal(self.hit_sequence)
-                else:    
-                    rospy.logwarn('The motion is busy. Try again later.')
-                    self.response_pub.publish('The motion is busy. Try again later.')
+               self.play()
             else:
                 rospy.logwarn('No notes to play. Say reed to read notes.')
                 self.response_pub.publish('No notes to play. Say reed to read notes.')
                 
         # pre play, use sound interpreted by computer
-        elif command.data == 'Marimbabot preview':
+        elif command == 'Marimbabot preview':
             # if a note sequence has been read via the 'read' command
             if self.sentence:
-                rospy.loginfo(f"playing audio preview of notes: {self.sentence}")
-
-                def audio_from_lilypond_client_thread():
-                    # Waits until the action server has started up and started
-                    self.lilypond_audio_client.wait_for_server()
-                    # Sends the goal to the action server.
-                    self.lilypond_audio_client.send_goal(LilypondAudioGoal(lilypond_string=String(data = self.sentence)))
-                    # Waits for the server to finish performing the action.
-                    # Includes that the audio file is generated and was played
-                    self.lilypond_audio_client.wait_for_result()
-                    # Prints out the result of executing the action
-                    rospy.logdebug(f"Result from audio_from_lilypond action server: {self.lilypond_audio_client.get_result()}")
-                    # Check if we have a success
-                    if not self.lilypond_audio_client.get_result().success:
-                        self.response_pub.publish('The audio preview could not be played. Try again later.')
-
-                # start thread to not block the main thread if the action server is not currently active
-                if self.lilypond_audio_client.simple_state == actionlib.SimpleGoalState.DONE:
-                    threading.Thread(target=audio_from_lilypond_client_thread).start()
-                else:
-                    rospy.logwarn('Preview is busy. Try again later.')
-                    self.response_pub.publish('Preview is busy. Try again later.')
+                self.preview()
             else:
                 rospy.logwarn('No notes to preview. Say reed to read notes.')
                 self.response_pub.publish('No notes to preview. Say reed to read notes.')
 
+        elif command == 'Marimbabot stop preview':
+            rospy.logwarn('Stopping preview.')
+            self.response_pub.publish('')
+
         # increase/decrease the tempo of the active notes
-        elif command.data == 'Marimbabot play faster' or 'Marimbabot play slower':
+        elif command == 'Marimbabot play faster' or command == 'Marimbabot play slower':
             # check if a note sequence has been read via the 'read' command
             if self.sentence:
-                tempo = re.findall('\\tempo 4 = (.*)', self.sentence)
-                if len(tempo) > 0:
-                    self.sentence = re.sub('\\tempo 4 = (.*)', '\\tempo 4 = {} '.format(str(int(tempo[0]) + 20)), self.sentence) if command.data == 'faster' else re.sub('\\tempo 4 = (.*)', '\\tempo 4 = ' + str(int(tempo[0]) - 20), self.sentence)
-                else:
-                    self.sentence = '\\tempo 4 = 80' + self.sentence if command.data == 'faster' else '\\tempo 4 = 40' + self.sentence
-                rospy.loginfo(f"updated notes: {self.sentence}")
-                self.update_hit_sequence()
-                rospy.logwarn('Tempo increased.' if command.data == 'faster' else 'Tempo decreased.')
-                self.response_pub.publish('Tempo increased.' if command.data == 'faster' else 'Tempo decreased.')
+                self.change_tempo(faster=True if command == 'Marimbabot play faster' else False)
+                self.play()
+            else:
+                rospy.logwarn('No notes to play. Say reed to read notes.')
+                self.response_pub.publish('No notes to play. Say reed to read notes.')
+
+        elif command == 'Marimbabot preview faster' or command == 'Marimbabot preview slower':
+            # check if a note sequence has been read via the 'read' command
+            if self.sentence:
+                self.change_tempo(faster=True if command == 'Marimbabot preview faster' else False)
+                self.preview()
             else:
                 rospy.logwarn('No notes to play. Say reed to read notes.')
                 self.response_pub.publish('No notes to play. Say reed to read notes.')
