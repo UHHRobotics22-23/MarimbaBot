10a8fb708636265f49a8442eb4bf2135f04f3d78
wang <yunlong.wang.de@outlook.com>
2023-07-06 13:52:03 +0200

    score calculation

--- marimbabot_audio/examples/send_hit_sequence.py
+++ marimbabot_audio/examples/send_hit_sequence.py
@@ -1,13 +1,15 @@
 from marimbabot_msgs.msg import HitSequence as HitSequenceMsg
 from marimbabot_msgs.msg import HitSequenceElement as HitSequenceElementMsg
 from marimbabot_msgs.msg import SequenceScore as SequenceScoreMsg
+from marimbabot_msgs.msg import NoteOnset as NoteOnsetMsg
 import numpy as np
 import rospy
-def gererate_hit_sequence_msg(seq:list):
-	msg = HitSequenceMsg()
-	msg.header.stamp = rospy.Time.now()
-	msg.sequence_id = 0
-	msg.hit_sequence_elements = []
+def gererate_sequence_msg(seq:list,id=0,noisy=True):
+	msgs_audio = []
+	msgs_vision = HitSequenceMsg()
+	msgs_vision.header.stamp = rospy.Time.now()
+	msgs_vision.sequence_id = id
+	msgs_vision.hit_sequence_elements = []
 	time_start = rospy.Time.now()
 	for idx, each in enumerate(seq):
 		hit = HitSequenceElementMsg()
@@ -18,25 +20,56 @@ def gererate_hit_sequence_msg(seq:list):
 		duration tone_duration
 		float32 loudness
 		'''
+		# build test msg for vision
 		hit.tone_name = each[:-1]
-		hit.octave = each[-1]
-		interval = rospy.Duration(0.5)
-		hit.start_time = time_start + interval* idx
-		hit.tone_duration = 0.5 + np.random.uniform(-0.2,0.2)
-		hit.loudness = 0.5 + np.random.uniform(-0.2,0.2)
-		msg.seq.append(hit)
-	return msg
+		hit.octave = int(each[-1])
+		interval = rospy.Duration(0.5*idx)
+		hit.start_time = time_start + interval
+		hit.tone_duration = rospy.Duration(0.5)
+		hit.loudness = 0.5
 
-def send_hit_sequnce():
+		if noisy:
+			hit.start_time += rospy.Duration(np.random.uniform(-0.2, 0.2))
+			hit.tone_duration += rospy.Duration(np.random.uniform(-0.2, 0.2))
+			hit.loudness += np.random.uniform(-0.2, 0.2)
+
+		msgs_vision.hit_sequence_elements.append(hit)
+
+		# build test msg for audio
+		msg_audio = NoteOnsetMsg()
+		msg_audio.header.stamp = time_start + interval
+		# msg_audio.header.stamp = time_start + interval + rospy.Duration(2)
+		msg_audio.loudness = 0.8
+		msg_audio.confidence = 0.8
+		msg_audio.duration = 0.5
+		if noisy:
+			msg_audio.header.stamp += rospy.Duration(np.random.uniform(-0.2,0.2))
+			msg_audio.duration += np.random.uniform(-0.2,0.2)
+
+		msg_audio.note = each
+		msgs_audio.append(msg_audio)
+
+	return msgs_audio, msgs_vision
+
+def send_hit_sequnce4test():
 	seq = ['C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'G4', 'G#4', 'A4', 'B4', 'C5']
-	rospy.init_node('send_hit_sequnce', anonymous=True)
-	pub = rospy.Publisher('/audio/hit_sequence', HitSequenceMsg, queue_size=10)
-	rate = rospy.Rate(0.5) # 10hz
+	vision_pub = rospy.Publisher('/audio/hit_sequence', HitSequenceMsg, queue_size=10)
+	audio_pub = rospy.Publisher('/audio/onset_notes', NoteOnsetMsg, queue_size=10)
+
+	rate = rospy.Rate(10)
+	n = 0
 	while not rospy.is_shutdown():
-		msg = gererate_hit_sequence_msg(seq)
-		pub.publish(msg)
-		rate.sleep()
+		msgs_audio, msgs_vision = gererate_sequence_msg(seq,n)
+		rospy.sleep(2)
+		vision_pub.publish(msgs_vision)
+		rospy.logdebug('publish vision msg')
+		for each in msgs_audio:
+			audio_pub.publish(each)
+			rate.sleep()
+		rospy.sleep(3)
+		n += 1
 
 if __name__ == '__main__':
-	rospy.init_node('send_hit_sequnce', anonymous=True)
-	send_hit_sequnce()
+	rospy.init_node('send_hit_sequnce', log_level=rospy.DEBUG)
+	send_hit_sequnce4test()
+	# rospy.spin()
--- marimbabot_audio/scripts/score.py
+++ marimbabot_audio/scripts/score.py
@@ -1,11 +1,19 @@
 import rospy
 from marimbabot_msgs.msg import HitSequence as HitSequenceMsg
 from marimbabot_msgs.msg import SequenceScore as SequenceScoreMsg
+from marimbabot_msgs.msg import NoteOnset as NoteOnsetMsg
+
 
 class Measurement():
 	def __init__(self):
-		self.notes_seq_audio = []
+		self.note_seq_audio = []
 		self.note_seq_vision = []
+		# start and end time of the sequnce to be measured
+		self.plan_start_time = None
+		self.plan_end_time = None
+		self.vision_seq_id = None
+		self.note_duration = None
+		self.init_ros()
 
 	def init_ros(self):
 		self.seq_sub = rospy.Subscriber(
@@ -19,16 +27,97 @@ class Measurement():
 			SequenceScoreMsg,
 			queue_size=10,
 			tcp_nodelay=True)
+		self.note_sub = rospy.Subscriber(
+			'/audio/onset_notes',
+			NoteOnsetMsg,
+			self.note_sequence_callback,
+			queue_size=10,
+			tcp_nodelay=True)
+
+	def reset(self):
+		self.note_seq_audio = []
+		self.note_seq_vision = []
+		self.plan_start_time = None
+		self.plan_end_time = None
+		self.vision_seq_id = None
+		self.note_duration = None
+
+	def note_sequence_callback(self, msg, time_tolerance=1):
+		if self.plan_start_time is not None:
+			if msg.header.stamp > self.plan_start_time - rospy.Duration(
+					time_tolerance) and msg.header.stamp < self.plan_end_time + rospy.Duration(time_tolerance):
+				self.note_seq_audio.append(msg)
 
 	def hit_sequence_callback(self, msg):
-		seq_id = msg.seq_id
-		seq = msg.seq
-		this_satrt = rospy.Time.now()
-		total_time = 0
-		for hit in seq:
-			total_time += hit.duration
-		this_end = this_satrt + rospy.Duration(total_time)
+		self.reset()
+		self.note_seq_vision = msg.hit_sequence_elements
+		self.note_duration = self.note_seq_vision[0].tone_duration
+		self.vision_seq_id = msg.sequence_id
+		self.plan_start_time = msg.header.stamp
+		self.plan_end_time = len(msg.hit_sequence_elements) * self.note_duration + self.plan_start_time
 
-	def extract_seq_from_audio_dectection(self):
-		pass
+	def is_time_for_comparison(self):
+		'''
+			Checks if the sequence is ready for comparison
+		'''
+		if len(self.note_seq_audio) != 0 \
+				and rospy.Time.now() > self.plan_end_time+rospy.Duration(2) \
+				and self.note_seq_audio[-1].header.stamp > self.plan_end_time:
 
+			self.plan_start_time = None
+			self.plan_end_time = None
+			return True
+		else:
+			return False
+
+	def compare(self, time_tolerance=1):
+		'''
+			Compare the target sequence with the extracted sequence, and return the recall rate.
+		'''
+		length = len(self.note_seq_vision)
+		success_times = 0
+		for hit_element_msg in self.note_seq_vision:
+			start_time_plan = hit_element_msg.start_time
+			start_time_plan_min = start_time_plan - rospy.Duration(time_tolerance)
+			start_time_plan_max = start_time_plan + rospy.Duration(time_tolerance)
+			for note_msg in self.note_seq_audio:
+				if note_msg.header.stamp > start_time_plan_min and note_msg.header.stamp < start_time_plan_max:
+					if note_msg.note == hit_element_msg.tone_name + str(hit_element_msg.octave):
+						success_times += 1
+						break
+		return success_times / length
+
+	def score_calculatioin(self):
+		'''
+			Waits for the sequence to be detected and then compares the sequence with the target sequence
+		'''
+		rate = rospy.Rate(10)  # 10hz
+		while not rospy.is_shutdown():
+			if self.is_time_for_comparison():
+				# print the sequence to be compared
+				audio_print = [f"{msg.note},{msg.header.stamp.to_sec()}" for msg in self.note_seq_audio]
+				vision_print = [f"{msg.tone_name + str(msg.octave)},{msg.start_time.to_sec()}" for msg in
+				                self.note_seq_vision]
+				rospy.logdebug('-------------------')
+				rospy.logdebug(f'got audio len:{len(self.note_seq_audio)}  vision len:{len(self.note_seq_vision)}')
+				rospy.logdebug(f'extracted audio seq: {audio_print}')
+				rospy.logdebug(f'received vision seq: {vision_print}')
+				if len(self.note_seq_vision) != len(self.note_seq_vision):
+					rospy.logwarn('The length of the received sequence and the target sequence are different.')
+
+				score = self.compare()
+				msg = SequenceScoreMsg()
+				msg.sequence_id = self.vision_seq_id
+				msg.score = score
+				msg.header.stamp = rospy.Time.now()
+				self.score_pub.publish(msg)
+				self.reset()
+				rospy.logdebug(f'Publish Score: ID: {msg.sequence_id} Score(recall): {msg.score}')
+			rate.sleep()
+
+
+if __name__ == '__main__':
+	rospy.init_node('score_calculator', log_level=rospy.DEBUG)
+	measurement = Measurement()
+	measurement.score_calculatioin()
+	rospy.spin()
