c195076778dd37eb01c4bb3258ed34fca15ccf82
wang <yunlong.wang.de@outlook.com>
2023-08-16 18:05:07 +0200

    update

---
+++ marimbabot_speech/src/keyowrd_spotting.py
@@ -0,0 +1,202 @@
+import os
+from nvidia import cudnn
+
+os.environ['LD_LIBRARY_PATH'] = f'{os.path.dirname(cudnn.__file__)}/lib:$LD_LIBRARY_PATH'
+# since I haven't specified the version of TensorFlow, it seems to install the newest version,
+# so according to official instruction, the environment needs to be configured.
+from precise_runner import PreciseEngine, PreciseRunner, ReadWriteStream
+from audio_common_msgs.msg import AudioDataStamped,AudioData
+from marimbabot_msgs.srv import SpeechRecognition,SpeechRecognitionResponse
+from std_msgs.msg import Float32
+from sound_play.msg import SoundRequest
+from sound_play.libsoundplay import SoundClient
+import rospy
+import webrtcvad
+
+VAD_ON = False
+KWS_ON = True
+
+class TTS:
+	def __init__(self):
+		self.soundhandle = SoundClient()
+		self.voice = 'voice_kal_diphone'
+		self.volume = 1.0
+
+	def say(self, text, blocking=False):
+		self.soundhandle.say(text, self.voice, self.volume, blocking=blocking)
+
+
+class KeywordSpotting:
+	"""
+		Keyword spotting is a class to detect the keyword from the audio stream.
+		It is based on the precise-engine, which is a RNN-based lightweight wake word listener.
+	"""
+
+	def __init__(self,
+	             sensitivity=0.5,
+	             log_level=rospy.DEBUG,
+	             engine_path=None,
+	             model_path=None,
+	             silence_t=1):
+		self.log_level = log_level
+		self.sensitivity = sensitivity
+		self.stream = None
+		self.init_audio()
+		try:
+			rospy.wait_for_service('/speech_node/speech_recognition', timeout=20)  # STT must on before KWS
+		except rospy.ROSException:
+			rospy.logerr("STT service is not available!")
+			raise rospy.ROSException("STT service is not available!")
+
+		self.init_kws(engine_path, model_path)
+		self.init_wad(silence_t=silence_t)
+		self.tts = TTS()
+		self.warmup()
+
+
+	def init_audio(self):
+		self.buffer = b''  # buffer for audio data
+		# subscribe to the audio stream from ros
+		self.audio_sub = rospy.Subscriber('/speech_node/audio_stamped', AudioDataStamped, self.audio_stream_callback,
+		                                  queue_size=500, tcp_nodelay=True)
+		self.sr = 16000  # sample rate
+
+	def init_kws(self, engine_path, model_path, sensitivity=0.5, trigger_level=5):
+		# use ReadWriteStream to forward the audio stream from ros to precise-engine
+		self.stream = ReadWriteStream()  # 16000 Hz 1 channel int16 audio
+		# the engine is the binary file to load
+		self.engine = PreciseEngine(engine_path, model_path)
+		self.runner = PreciseRunner(
+			# Object containing info on the binary engine
+			engine=self.engine,
+			# From 0.0 to 1.0, how sensitive the network should be
+			sensitivity=sensitivity,
+			# Number of chunk activations needed to trigger on_activation
+			trigger_level=trigger_level,
+			# callback for when the wake word is heard
+			on_activation=self.on_activation,
+			# callback for every new prediction
+			on_prediction=self.on_prediction,
+			# Binary audio stream to read 16000 Hz 1 channel int16 audio from. If not given, the microphone is used
+			stream=self.stream)
+		if self.log_level == rospy.DEBUG:
+			self.prob_pub = rospy.Publisher('/speech_node/keyword_spotting/prob', Float32, queue_size=20)
+
+	# init the human voice activity detection
+	def init_wad(self, silence_t):
+		self.sentence_start_t = None
+		self.silence_t = silence_t
+		self.silence_start_t = None
+		self.max_sentence_t = 10
+		self.vad = webrtcvad.Vad(3)  # 0: highest, 1: high, 2: middle, 3: low (default)
+
+	def reset(self):
+		self.buffer = b''
+		self.silence_start_t = None
+		self.sentence_start_t = None
+		global VAD_ON
+		global KWS_ON
+		VAD_ON = False
+		KWS_ON = True
+
+
+	def start(self):
+		self.runner.start()
+
+	# use the sound_play to let the robot say something
+	def say(self, text):
+		self.tts.say(text,blocking=True)
+		rospy.logdebug(f"Say: {text}")
+
+	# triggered for keyword activation, condition refer to the trigger_level
+	def on_activation(self):
+		global KWS_ON
+		global VAD_ON
+		# while not KWS_ON:
+		# 	pass
+		if self.log_level == rospy.DEBUG:
+			rospy.logdebug("Keyword Spotting is activated.")
+		self.say("Yes?")
+		VAD_ON = True
+		KWS_ON = False
+
+	def warmup(self):
+		pass
+
+	# triggered for each prediction
+	def on_prediction(self, prob):
+		rospy.loginfo_once("Keyword Spotting is on.")
+		if prob > self.sensitivity:
+			self.prob_pub.publish(prob)
+
+	def stop(self):
+		self.runner.stop()
+
+	def __del__(self):
+		self.runner.stop()
+		self.runner = None
+		self.engine = None
+
+	def call_speech_recognition_service(self, buffer):
+		try:
+			rospy.wait_for_service('/speech_node/speech_recognition',timeout=4)
+		except rospy.ROSException as e:
+			rospy.logwarn("Speech Recognition Service is not on: %s" % e)
+			return None
+		audio_data = AudioData()
+		audio_data.data = buffer  # the buffer is the audio data, same data type as the audio stream from topic /speech_node/audio_stamped
+		try:
+			speech_recognition = rospy.ServiceProxy('/speech_node/speech_recognition', SpeechRecognition)
+			resp = speech_recognition(audio_data)
+			return resp.text
+		except rospy.ServiceException as e:
+			print("Service call failed: %s" % e)
+
+
+	def audio_stream_callback(self, msg):
+		global KWS_ON
+		global VAD_ON
+
+		if VAD_ON:
+			self.buffer += msg.audio.data
+			# If the listening is longer than the max_sentence_t(10 sec as default),
+			# then trigger the speech recognition.
+			# It could cut off the speaking in the middle, but it is better than waiting for a long time,
+			# since VAD is kind of sensitive.
+			if self.sentence_start_t is None:
+				self.sentence_start_t = msg.header.stamp
+			else:
+				if msg.header.stamp - self.sentence_start_t > rospy.Duration.from_sec(self.max_sentence_t):
+					text = self.call_speech_recognition_service(self.buffer)
+					# TODO call command exraction service
+					self.reset()
+					return
+
+			# use the VAD to detect the silence, if the silence is longer than the silence_t, then trigger the speech recognition
+			if self.vad.is_speech(msg.audio.data, self.sr):
+				self.silence_start_t = msg.header.stamp
+			# if the silence is longer than the silence_t, then trigger the speech recognition
+			elif self.silence_start_t is not None and \
+					msg.header.stamp - self.silence_start_t > rospy.Duration.from_sec(self.silence_t):
+					text = self.call_speech_recognition_service(self.buffer)
+					# TODO call command exraction service
+					self.reset()
+					return
+
+		if KWS_ON and self.stream is not None:
+			# froward th stream from ros to precise-engine
+			self.stream.write(msg.audio.data)
+
+
+
+if __name__ == '__main__':
+	log_level = rospy.DEBUG
+	rospy.init_node('keyword_spotting_node', anonymous=True, log_level=log_level)
+	engine_path_default = "/home/wang/workspace/marimbabot_ws/env_this/bin/precise-engine"
+	model_path_default = '/home/wang/workspace/marimbabot_ws/kws_ws/src/marimbabot/marimbabot_speech/hi-marimbabot.pb'
+	# start the keyword spotting
+	engine_path = rospy.get_param('~engine_path', default=engine_path_default)
+	model_path = rospy.get_param('~model_path', default=model_path_default)
+	keyword_spotting = KeywordSpotting(engine_path=engine_path, model_path=model_path, log_level=log_level)
+	keyword_spotting.start()
+	rospy.spin()
--- marimbabot_speech/src/speech2text.py
+++ marimbabot_speech/src/speech2text.py
@@ -1,85 +1,124 @@
 import os
 from nvidia import cudnn
-os.environ['LD_LIBRARY_PATH'] = f'{os.path.dirname(cudnn.__file__)}/lib:$LD_LIBRARY_PATH'
-# since I haven't specified the version of TensorFlow, it seems to install the newest version, so according to official instruction, the environment needs to be configured.
 
+os.environ['LD_LIBRARY_PATH'] = f'{os.path.dirname(cudnn.__file__)}/lib:$LD_LIBRARY_PATH'
+# since I haven't specified the version of TensorFlow, it seems to install the newest version,
+# so according to official instruction, the environment needs to be configured.
+from precise_runner import PreciseEngine, PreciseRunner, ReadWriteStream
+from audio_common_msgs.msg import AudioDataStamped
+from marimbabot_msgs.msg import Command as CommandMsg
+from marimbabot_msgs.msg import Speech as SpeechMsg
+from marimbabot_msgs.srv import SpeechRecognition,SpeechRecognitionResponse
 import rospy
 import whisper
-from utils.file_control import WAVFile
-from marimbabot_msgs.msg import Speech as SpeechMsg
-from marimbabot_msgs.msg import TmpFile as TmpFileMsg
-import time
+import webrtcvad
+import numpy as np
+import struct
 from utils.command import get_commands
 
+
+# Speech to text recognition, it is a wrapper of whisper
 class STT:
-	def __init__(self):
-		# 'tiny.en', 'tiny', 'base.en', 'base', 'small.en', 'small', 'medium.en', 'medium', 'large-v1', 'large-v2', 'large'
-		self.model = whisper.load_model('medium.en')
-		self.file_controller = WAVFile()
-		self.speech_pub = rospy.Publisher('/speech_node/speech', SpeechMsg, queue_size=100, tcp_nodelay=True)
-		self.tmp_sub = rospy.Subscriber('/speech_node/audio_tmp', TmpFileMsg, self.tmp_callback, queue_size=10, tcp_nodelay=True)
-		self.recognize_freq = 2  # Hz
-		self.recognize_rate = rospy.Rate(self.recognize_freq)
-		self.no_speech_prob_filter = 0.5
-		rospy.logdebug(f"cwd:{os.getcwd()}")
+	def __init__(self, log_level=rospy.INFO):
+		self.log_level = log_level
 		self.prompt = self.generate_prompt()
 
+		self.init_audio_config()
+		self.init_whisper_model()
+		self.warm_up()  # warm up the whisper model
+		self.init_ros_node()
+
+
+	def init_audio_config(self):
+		self.sr = rospy.get_param('~sr', 16000)
+
+	def init_whisper_model(self):
+		# 'tiny.en', 'tiny', 'base.en', 'base', 'small.en', 'small', 'medium.en', 'medium', 'large-v1', 'large-v2', 'large'
+		self.model = whisper.load_model('medium.en')
+
+	def warm_up(self):
+		# 30 denote sec, since whisper take fixed length of audio data as input
+		self.recognize(np_data=np.zeros(self.sr*30, dtype=np.float32),pub_speech=False)
+		rospy.loginfo("Whisper model is warmed up!")
+
+	def init_ros_node(self):
+		rospy.Service('speech_recognition', SpeechRecognition, self.handle_speech2text_service)
+		# To publish the recognized text
+		self.speech_pub = rospy.Publisher(
+			'speech',
+			SpeechMsg,
+			queue_size=100,
+			tcp_nodelay=True)
+
+	def handle_speech2text_service(self, req):
+		if self.log_level == rospy.DEBUG:
+			rospy.logdebug(f"STT service triggered.")
+		text, no_speech_prob = self.recognize(np_data=self.unpack_stream(req.audio.data))
+		return SpeechRecognitionResponse(text=text, no_speech_prob=no_speech_prob)
+
+	# To address the way how to unpack the data for whisper
+	def unpack_stream(self, data):
+		return np.array(struct.unpack(f"{int(len(data) / 2)}h", bytes(data)), dtype=float) / 526
+
 	def generate_prompt(self):
-		commands = get_commands()
-		base_prompt = '''
-			Marimbabot is a marimba playing robot arm. You are able to give it commands, if you confuse just give it "None". The possible commands include:		
-		'''
-		for command in commands:
-			base_prompt += ''.join(f'{command.strip()}, ')
-		rospy.logdebug(f"Generate prompt as: {base_prompt}.")
-		return base_prompt
-
-
-	def tmp_callback(self, tmp_file_msg):
-		file_path = tmp_file_msg.file_path
-		text, no_speech_prob = self.recognize(file_path)
-		if no_speech_prob > self.no_speech_prob_filter:
-			return
-		if len(text)>300:
-			return
-		speech_msg = SpeechMsg()
-		speech_msg.header.stamp = rospy.Time.now()
-		speech_msg.speech = text
-		speech_msg.is_finished = tmp_file_msg.is_finished
-		self.speech_pub.publish(speech_msg)
-		rospy.logdebug(f"speech published.")
-
-
-	def recognize(self, file_path:str):
-		time_0 = time.time()
-		# load audio and pad/trim it to fit 30 seconds
-		audio = whisper.load_audio(file_path)
-		audio = whisper.pad_or_trim(audio)
-
-		# make log-Mel spectrogram and move to the same device as the model
-		mel = whisper.log_mel_spectrogram(audio).to(self.model.device)
-
-		# decode the audio
-		options = whisper.DecodingOptions(fp16=True, language='en',prompt=self.prompt)
-		time_1 = time.time()
-		rospy.logdebug('*'*30)
-		rospy.logdebug(f"prerpocesing time:{time_1-time_0}")
-		result = whisper.decode(self.model, mel, options)
-		rospy.logdebug(f"decoding time:{time.time()-time_1}")
-		text = result.text
-		no_speech_prob = result.no_speech_prob
-		rospy.logdebug(f"no_speech_prob: {no_speech_prob}")
-		rospy.logdebug(f" TEXT:{text}")
-		return text, no_speech_prob
+		# commands = get_commands()
+		# base_prompt = '''
+		# 	Marimbabot is a marimba playing robot arm. You are able to give it commands, if you confuse just give it "None". The possible commands include:
+		# '''
+		# for command in commands:
+		# 	base_prompt += ''.join(f'{command.strip()}, ')
+		# rospy.logdebug(f"Generate prompt as: {base_prompt}.")
+		# return base_prompt
+		return "Marimbabot is a instrument playing robot arm. You are able to give it several common robot's commands"
 
 	def run(self):
 		rospy.spin()
 
+	def recognize(self, file_path=None, np_data=None, pub_speech=True):
+		time_0 = rospy.Time.now()
+		# load audio and pad/trim it to fit 30 seconds
+		if file_path is not None:
+			audio = whisper.load_audio(file_path)
+		elif np_data is not None:
+			audio = np_data.astype(np.float32)
+		else:
+			rospy.logerr("No audio data is provided! Either file_path or np_data should be provided!")
+			return
+
+		audio = whisper.pad_or_trim(audio)  # each time feed 16000(sample rate)*30(sec) data samples to whisper model
+		# make log-Mel spectrogram and move to the same device as the model
+		mel = whisper.log_mel_spectrogram(audio).to(self.model.device)
+
+		# decode the audio
+		options = whisper.DecodingOptions(fp16=True, language='en')
+		time_1 = rospy.Time.now()
+		result = whisper.decode(self.model, mel, options)
+		# options = whisper.DecodingOptions(fp16=True, language='en',prompt=self.prompt)
+		time_2 = rospy.Time.now()
+		text = result.text
+		no_speech_prob = result.no_speech_prob
+		if self.log_level == rospy.DEBUG:
+			rospy.logdebug('*' * 30)
+			rospy.logdebug(f"Pre-processing time:{(time_1 - time_0).to_sec():.4f}")
+			rospy.logdebug(f"Prediction time:{(time_2 - time_1).to_sec():.4f}")
+			rospy.logdebug(f"Result: [{text}]")
+			rospy.logdebug(f"No_speech_prob: {no_speech_prob:.4f}")
+		if pub_speech:
+			# publish the recognized text
+			self.speech_pub.publish(
+				SpeechMsg(
+					header=rospy.Time.now(),
+					text=text,
+					no_speech_prob=no_speech_prob)
+			)
+		return text, no_speech_prob
+
 if __name__ == '__main__':
-	rospy.init_node('speech_stt_node', log_level=rospy.DEBUG)
-	speech_recognition = STT()
-	speech_recognition.run()
+	# TODO: Finish the command extraction
+	# TODO: reset the way to load precise-engine
+	# TODO: consider the voice from other group members, i.e. change the dataset and retrain the model
+	log_level = rospy.DEBUG
+	rospy.init_node('speech_recognition_node', log_level=log_level)
+	sst = STT(log_level=log_level)
+	sst.run()
 
-
-	# speech_recognition.generate_prompt()
-	# rospy.spin()
