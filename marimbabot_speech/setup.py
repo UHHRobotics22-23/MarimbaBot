7f9afc00ea580b544495b3b41795888cf9af87e6
wang <yunlong.wang.de@outlook.com>
2023-08-22 14:48:39 +0200

    fix

--- marimbabot_speech/setup.py
+++ marimbabot_speech/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
-    packages=['utils'],
+    packages=[''],
     package_dir={'': 'src'}
 )
 setup(**d)
